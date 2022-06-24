aa37e7b9616b359504980f31b14179401e9f96f1
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-24 14:06:50 +0300

    Example6 special username

---
+++ example6_HelloUserNew/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Hello, who are you?");
+string username = Console.ReadLine();
+if (username.ToLower() == "dima")
+{
+    Console.WriteLine("Hello my dear!");
+}
+else
+{
+    Console.WriteLine("Hello");
+    Console.WriteLine(username);
+    Console.WriteLine("Where Dima?!");
+}
